edc7d0e1caaa5e68a2f7e1b1dfaced97dfce39c1
Alex Romo <ayromo08@gmail.com>
2020-02-13 05:57:31 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,10 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
-counter = 0
 def play(board)
-  counter += 1
-  until counter == 9
+  counter = 0
+  while counter < 9
     turn(board)
+    counter += 1
   end
 end
